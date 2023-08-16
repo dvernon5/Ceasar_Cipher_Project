99b8b8152b4e31013b090067730ee35d00adb0a4
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-08-15 22:21:14 -0700

    Implement a map to hold char and integer
    Implemented a map that map characters to Integers. As well, 
    Implemented a map that map integers to characters.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <map>
 using namespace std;
 
 const int leftShift = 3;
 const int numberInAlphabets = 26;
+
+map<char, int> charToIntMapping = {
+    {'A', 0}, {'B', 1}, {'C', 2}, {'D', 3}, {'E', 4},
+    {'F', 5}, {'G', 6}, {'H', 7}, {'I', 8}, {'J', 9},
+    {'K', 10}, {'L', 11}, {'M', 12}, {'N', 13}, {'O', 14},
+    {'P', 15}, {'Q', 16}, {'R', 17}, {'S', 18}, {'T', 19},
+    {'U', 20}, {'V', 21}, {'W', 22}, {'X', 23}, {'Y', 24}, {'Z', 25}
+};
+
+map<int, char> intToCharMapping = {
+    {'A', 0}, {'B', 1}, {'C', 2}, {'D', 3}, {'E', 4},
+    {'F', 5}, {'G', 6}, {'H', 7}, {'I', 8}, {'J', 9},
+    {'K', 10}, {'L', 11}, {'M', 12}, {'N', 13}, {'O', 14},
+    {'P', 15}, {'Q', 16}, {'R', 17}, {'S', 18}, {'T', 19},
+    {'U', 20}, {'V', 21}, {'W', 22}, {'X', 23}, {'Y', 24}, {'Z', 25}
+};
 
 int characterToInteger(char letter);
 
