7f4468fa2ecd8f978a652aea15e022788dd34d13
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-08-16 14:09:37 -0700

    Updated Caesar Cipher Final Version

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
 #include <map>
-using namespace std;
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::map;
+using std::endl;
 
 const int leftShift = 3;
 const int numberInAlphabets = 26;
@@ -15,21 +20,17 @@ map<char, int> charToIntMapping = {
 };
 
 map<int, char> intToCharMapping = {
-    {'A', 0}, {'B', 1}, {'C', 2}, {'D', 3}, {'E', 4},
-    {'F', 5}, {'G', 6}, {'H', 7}, {'I', 8}, {'J', 9},
-    {'K', 10}, {'L', 11}, {'M', 12}, {'N', 13}, {'O', 14},
-    {'P', 15}, {'Q', 16}, {'R', 17}, {'S', 18}, {'T', 19},
-    {'U', 20}, {'V', 21}, {'W', 22}, {'X', 23}, {'Y', 24}, {'Z', 25}
+    {0, 'A'}, {1, 'B'}, {2, 'C'}, {3, 'D'}, {4, 'E'},
+    {5, 'F'}, {6, 'G'}, {7, 'H'}, {8, 'I'}, {9, 'J'},
+    {10, 'K'}, {11, 'L'}, {12, 'M'}, {13, 'N'}, {14, 'O'},
+    {15, 'P'}, {16, 'Q'}, {17, 'R'}, {18, 'S'}, {19, 'T'},
+    {20, 'U'}, {21, 'V'}, {22, 'W'}, {23, 'X'}, {24, 'Y'}, {25, 'Z'}
 };
 
 int characterToInteger(char letter);
-
-int shiftedCharacter(char letter, int shift);
-
+char shiftedCharacter(char letter, int leftshift);
 string encryptMessage(string plainText);
-
-string decrypMessage(string ciphertext);
-
+string decryptMessage(string ciphertext);
 void displayDashLine();
 
 int main(void)
@@ -57,40 +58,45 @@ int main(void)
 int characterToInteger(char letter)
 {
     auto itr = charToIntMapping.find(letter);
-    if (itr != charToIntMapping.end()) 
+    
+    if (itr != charToIntMapping.end())
     {
         return itr->second;
     }
-    return -1; // Invalid character
+    
+    return -1;  // Letter is not found.
 }
 
-char shiftedCharacter(char letter, int shift) 
+char shiftedCharacter(char letter, int leftShift)
 {
     int characterInteger = characterToInteger(letter);
-    if (characterInteger >= 0) 
+    if (characterInteger >= 0)
     {
-        return intToCharMapping[(characterInteger + shift + numberInAlphabets) % numberInAlphabets];
+        return intToCharMapping[(characterInteger + leftShift + numberInAlphabets) % 26];
     }
+    
     return letter;
 }
 
-string encryptMessage(string plainText) 
+string encryptMessage(string plainText)
 {
     string ciphertext = "";
-    for (char c : plainText) 
+    for (auto c : plainText)
     {
-        ciphertext += shiftedCharacter(c, -leftshift);
+        ciphertext += shiftedCharacter(c, -leftShift);
     }
+    
     return ciphertext;
 }
 
-string decryptMessage(string ciphertext) 
+string decryptMessage(string plainText)
 {
-   string deciphertext = "";
-    for (char c : ciphertext) 
+    string deciphertext = "";
+    for (auto c : plainText)
     {
         deciphertext += shiftedCharacter(c, leftShift);
     }
+    
     return deciphertext;
 }
 
